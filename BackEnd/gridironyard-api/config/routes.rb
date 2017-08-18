44e91dd1ebf5e020e8909e506f1a58cbc8586d65
Joe Fenton <joe.e.fenton@gmail.com>
2017-08-18 11:45:22 -0400

    sessions and users

--- BackEnd/gridironyard-api/config/routes.rb
+++ BackEnd/gridironyard-api/config/routes.rb
@@ -3,4 +3,9 @@ Rails.application.routes.draw do
   get '/nfl_teams/team_cache', to: 'nfl_teams#team_cache', as: 'team_cache'
   get '/nfl_teams/stats_cache', to: 'nfl_teams#stats_cache'
   root 'nfl_teams#index'
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+  get '/signup'=> 'users#new'
+  post '/users' => 'users#create'
 end
