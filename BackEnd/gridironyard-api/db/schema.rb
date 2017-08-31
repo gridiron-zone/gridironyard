b181d646a2816c4011bfe32d8fd6485b5f5e1d15
Joshua Flack <joshu.alexander@gmail.com>
2017-08-31 14:36:12 -0400

    users

--- BackEnd/gridironyard-api/db/schema.rb
+++ BackEnd/gridironyard-api/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170824194047) do
+ActiveRecord::Schema.define(version: 20170831182722) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,6 +49,15 @@ ActiveRecord::Schema.define(version: 20170824194047) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "nfl_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.string "password"
+    t.string "picture"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "player_stats", "players"
